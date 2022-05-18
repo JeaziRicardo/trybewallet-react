c54e3a7bb80a53796f70847a9bdbbe5328f12e88
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-18 18:47:28 -0300

    Cria as rotas para Login e Wallet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class App extends Component {
   render() {
-    return <div>Hello, TrybeWallet!</div>;
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/carteira" component={ Wallet } />
+      </Switch>
+    );
   }
 }
 
