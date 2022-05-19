29ebf8ed3a44881cccd2daadc1d5ec52bc3661a0
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-19 15:59:17 -0300

    corrige index.js e user.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case CHANGE_EMAIL:
+  case 'CHANGE_EMAIL':
     return {
       ...state,
       email: action.payload,
