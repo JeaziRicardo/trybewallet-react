d4dfa3e329e0701223a314769974d961f993143d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-19 17:57:24 -0300

    Coloca as informações do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-
+      <header>
+        <h1>Trybe Wallet</h1>
+        <p>
+          Email:
+          {' '}
+          <span data-testid="email-field"> </span>
+        </p>
+        <p>
+          Despesa Total:
+          {' '}
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+      </header>
     );
   }
 }
