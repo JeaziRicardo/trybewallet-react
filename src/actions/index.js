cd1ebbb9a011e5d6992ccae5f318ff7db3ca8782
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-19 23:25:11 -0300

    [04] - Cria a função que recebe e trata os dados da API e envia para state global

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const walletAction = (state) => ({
   type: 'CHANGE_CURRENCY',
   payload: state,
 });
+
+export function fetchCurrencies() {
+  return async (dispatch) => {
+    const request = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const response = await request.json();
+    const currencies = Object.keys(response);
+    const currencyUp = currencies.filter((currency) => currency !== 'USDT');
+    dispatch(walletAction(currencyUp));
+  };
+}
