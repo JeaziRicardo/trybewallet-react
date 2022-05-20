ebdce8cb09de758095556d7471acf3fa94dd425d
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-19 23:52:32 -0300

    [04] - Escreve os dados das moedas no state global ao iniciar Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchCurrencies } from '../actions';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { changeCurrencies } = this.props;
+    changeCurrencies();
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +19,12 @@ class Wallet extends React.Component {
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  changeCurrencies: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  changeCurrencies: () => dispatch(fetchCurrencies()),
+});
+
+export default connect(null, mapDispatchToProps)(Wallet);
