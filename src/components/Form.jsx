ca9f6f456d5efe00988075fa4146fb75bb960588
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-20 21:41:15 -0300

    Cria o componente Form

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <main>
+        <form>
+
+        </form>
+      </main>
+    );
+  }
+}
+
+export default Form;
